6d287a9c24498abec4ae705efad9d4883572e0b4
infsolution <clsinfsolution@gmail.com>
2018-06-03 17:54:12 -0300

    updates

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,7 +8,7 @@
                 <div class="card" style="width: 32rem; height: 8rem; border: none">
                      
                     <div class="card-body">
-                        <h5 class="card-title"><a href="{{$n->url}}" >{{$n->title}} </a></h5>
+                        <h5 class="card-title"><a href="{{$n->url}}">{{$n->title}} </a></h5>
                         <p class="card-text">{{$n->url}}</p>
                     </div>
                      <!--img class="card-img-top" src="https://static.meionorte.com/uploads/imagens/2018/5/29/db582a89-df8d-4c40-b556-6c9e0acce37a.jpeg" alt="Card image"-->
